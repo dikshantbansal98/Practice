a7affaf9b283808cdd22cdb6a2c265c858bcb830
dikshant bansal <dikshantbansal98@gmail.com>
2022-02-05 13:45:54 +0530

    Time: 469 ms (52.16%), Space: 187.7 MB (5.92%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.cpp
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -1,33 +1,25 @@
 class DetectSquares {
 public:
-    map<pair<int,int>, int>mp;
-    
+    vector<vector<int>> frequencies;
+    vector<pair<int,int>>points;
     DetectSquares() {
-        
+        frequencies = vector<vector<int>>(1001, vector<int>(1001));
     }
     
     void add(vector<int> point) {
-        mp[{point[0], point[1]}]++;
+        frequencies[point[0]][point[1]]++;
+        if(frequencies[point[0]][point[1]] == 1)
+            points.push_back({point[0],point[1]});
     }
     
     int count(vector<int> point) {
-        
-        int ans = 0;
-        for(auto itr : mp)
-        {
-            pair<int,int>p = itr.first;
-            if(p.first != point[0] and p.second != point[1] and (abs(point[0] - p.first) == abs(point[1] - p.second)))
-            {
-                int first = mp[{p.first, p.second}];
-                int second = mp[{p.first, point[1]}];
-                int third = mp[{point[0], p.second}];
-                
-                int cur = first * second * third;
-                ans += cur;
+        int count = 0;
+        for(auto [x,y]: points) {
+            if(x != point[0] and y != point[1] and (abs(point[0] - x) == abs(point[1] - y))) {
+                count += (frequencies[point[0]][y]) * (frequencies[x][point[1]]) * (frequencies[x][y]);
             }
         }
-        
-        return ans;
+        return count;
     }
 };
 
