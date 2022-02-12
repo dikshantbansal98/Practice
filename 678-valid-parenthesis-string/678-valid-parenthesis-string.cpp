a294f1fc670a69c43c31d66628b0624d4ad5fa9d
dikshant bansal <dikshantbansal98@gmail.com>
2022-02-12 21:22:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (54.61%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -2,22 +2,13 @@ class Solution {
 public:
     vector<vector<int>>dp;
     bool checkValidString(string s) {
-        dp = vector<vector<int>>(s.size(), vector<int>(s.size()+1,-1));
-        return isValid(s, 0, 0);
-    }
-    bool isValid(string &s, int idx, int count) {
-        if(count < 0) {
-            return false;
-        }
-        if(idx == s.size())
-            return count == 0;
-        if(dp[idx][count]!=-1) {
-            return dp[idx][count];
-        }
-        if(s[idx] == '(')
-            return dp[idx][count] = isValid(s, idx + 1, count+1);
-        if(s[idx] == ')')
-            return dp[idx][count] = isValid(s, idx + 1, count-1);
-        return dp[idx][count] = isValid(s, idx+1, count-1) || isValid(s, idx+1, count) || isValid(s, idx+1, count+1);
+       int lo = 0, hi = 0;
+       for (char &c: s) {
+           lo += c == '(' ? 1 : -1;
+           hi += c != ')' ? 1 : -1;
+           if (hi < 0) return false;
+           lo = max(lo,0);
+       }
+       return lo <= 0;
     }
 };
