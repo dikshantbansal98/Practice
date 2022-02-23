94606a48e2be5f2db3f3eeee3f1b98902f2f191a
dikshant bansal <dikshantbansal98@gmail.com>
2022-02-23 23:01:24 +0530

    Time: 91 ms (28.89%), Space: 14.3 MB (37.83%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        k++;
+        vector<vector<int>>dp(n,vector<int>(k+1,1e9));
+        for(int i = 0; i <= k; ++i) {
+            dp[src][i] = 0;
+        }
+        vector<pair<int,int>>graph[n];
+        for(auto flight: flights) {
+            graph[flight[0]].push_back({flight[1],flight[2]});
+        }
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>>pq;
+        pq.push({0,src,k});
+        while(!pq.empty()) {
+            auto [distance, src, k] = pq.top(); pq.pop();
+            for(auto [to,weight]: graph[src]) {
+                if(k >= 1 && dp[to][k-1]>distance+weight) {
+                    dp[to][k-1] = distance+weight;
+                    pq.push({dp[to][k-1], to, k-1});
+                }
+            }
+        }
+        int cheapestCombination = 1e9;
+        for(int i = 0; i <= k; ++i) {
+            cheapestCombination = min(cheapestCombination, dp[dst][i]);
+        }
+        if(cheapestCombination == 1e9) {
+            return -1;
+        }
+        return cheapestCombination;
+    }
+};
