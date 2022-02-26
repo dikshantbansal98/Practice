80d3c96a3a53e6a9bb0d0a1607213dfc4099d611
dikshant bansal <dikshantbansal98@gmail.com>
2022-02-26 23:31:03 +0530

    Time: 71 ms (22.22%), Space: 19.3 MB (30.43%) - LeetHub

---
+++ 356-line-reflection/356-line-reflection.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isReflected(vector<vector<int>>& points) {
+        sort(points.begin(), points.end());
+        vector<vector<int>> newPoints;
+        for(auto point: points) {
+            if(newPoints.size() && (newPoints.back()[0] == point[0] && newPoints.back()[1] == point[1])) {
+                continue;
+            }
+            newPoints.push_back(point);
+        }
+        points = newPoints;
+        cout<<points.size()<<endl;
+        double value = 0;
+        for(auto &point: points) {
+            value += point[0];
+        }
+        value/=points.size();
+        map<double,map<double,double>>frequencies;
+        for(int i = 0; i < points.size(); ++i) {
+            frequencies[points[i][1]][value-points[i][0]]++;
+        }
+        for(auto &[y, frequency]: frequencies) {
+            for(auto &[dis,freq]: frequency) {
+                if(freq!=frequency[-dis]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
