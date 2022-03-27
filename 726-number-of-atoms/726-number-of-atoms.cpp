95abd35b4462b3f58699a31462ee015ba5d43a60
dikshant bansal <dikshantbansal98@gmail.com>
2022-03-27 13:51:34 +0530

    Time: 6 ms (29.48%), Space: 9 MB (5.67%) - LeetHub

--- 726-number-of-atoms/726-number-of-atoms.cpp
+++ 726-number-of-atoms/726-number-of-atoms.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
+    
     string countOfAtoms(string formula) {
         stack<map<string,int>>st;
         for(int i = 0; i < formula.size(); ++i) {
             if(is_upper(formula[i])) {
-                string str = "";
-                str += formula[i];
-                int j = i+1;
-                while(j<formula.size() && is_lower(formula[j])) {
-                    str += formula[j++];
-                }
-                int k = 1;
-                if(j<formula.size() && is_numeric(formula[j])) {
-                    k = 0;
-                    while(j<formula.size() && is_numeric(formula[j])) {
-                        k = k*10 + (formula[j++]-'0');
-                    }
-                }
-                i = j-1;
-            
+                auto [str, j] = firstOperation(i, formula);
+                int k = secondOperation(j, formula, i);
                 map<string,int>freq;
                 freq[str] = k;
                 st.push(freq);
@@ -28,44 +16,27 @@ public:
                 st.push(freq);
             }
             else if(formula[i] == ')') {
-                int j = i+1;
-                int k = 1;
-                if(j<formula.size() && is_numeric(formula[j])) {
-                    k = 0;
-                    while(j<formula.size() && is_numeric(formula[j])) {
-                        k = k*10 + (formula[j++]-'0');
-                    }
-                }
-                i = j-1;
-                map<string,int>result;
-                while(st.size() && st.top().size()) {
-                    map<string,int>freq = st.top(); st.pop();
-                    for(auto [element, frequency]: freq) {
-                    
-                        result[element] += frequency*k;
-                    }
-                }
-                
-                st.pop();
-                st.push(result);
+                int k = secondOperation(i+1, formula, i);
+                mergeFrequencies(st, k, true);
             }
             
         }
+        mergeFrequencies(st);
+        return getFinalOutput(st.top());
+    }
+    
+    void mergeFrequencies(stack<map<string,int>>&st, int k = 1, bool isOpen = false) {
         map<string,int>result;
-        while(st.size()) {
+        while(st.size() && st.top().size()) {
             map<string,int>freq = st.top(); st.pop();
             for(auto [element, frequency]: freq) {
-                result[element] += frequency;
+                result[element] += frequency*k;
             }
         }
-        string output = "";
-        for(auto [element, frequency]: result) {
-            output+=element;
-            if(frequency > 1) {
-                output += to_string(frequency);
-            }
+        if(isOpen) {
+            st.pop();
         }
-        return output;
+        st.push(result);
     }
     
     bool is_upper(char ch) {
@@ -78,5 +49,38 @@ public:
     
     bool is_numeric(char ch) {
         return ch >= '0' && ch <= '9';
+    }
+    
+    tuple<string,int> firstOperation(int i, string &formula) {
+        string str = "";
+        str += formula[i];
+        int j = i+1;
+        while(j<formula.size() && is_lower(formula[j])) {
+            str += formula[j++];
+        }
+        return {str,j};
+    }
+    
+    int secondOperation(int j, string &formula, int &i) {
+        int k = 1;
+        if(j<formula.size() && is_numeric(formula[j])) {
+            k = 0;
+            while(j<formula.size() && is_numeric(formula[j])) {
+                k = k*10 + (formula[j++]-'0');
+            }
+        }
+        i = j-1;
+        return k;
+    }
+    
+    string getFinalOutput(map<string,int>&result) {
+        string output = "";
+        for(auto [element, frequency]: result) {
+            output+=element;
+            if(frequency > 1) {
+                output += to_string(frequency);
+            }
+        }
+        return output;
     }
 };
