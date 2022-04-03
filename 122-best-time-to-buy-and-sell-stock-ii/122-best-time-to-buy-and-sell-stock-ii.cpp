3b2df651c8faa064f11c4e5074faa199e61b392d
dikshant bansal <dikshantbansal98@gmail.com>
2022-04-03 12:36:35 +0530

    Time: 20 ms (6.89%), Space: 13.1 MB (11.36%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,18 +4,13 @@ public:
     {
         if(prices.size()<2)
             return 0;
-        prices.push_back(0);
         vector<int>dp(prices.size()+1);
-        int minimum = 1e9, ans = 0;
-        for(int i = 0; i < prices.size()-1; ++i) {
-            if(prices[i]>minimum && prices[i]>prices[i+1]) {
-                ans += prices[i]-minimum;
-                minimum = 1e9;
-            }
-            else {
-                minimum = min(minimum, prices[i]);
-            }
-        }
-        return ans;
+    	int maxi = -prices[0],ans=0;
+    	for(int i = 1;i< prices.size();++i)
+    	{
+    		dp[i] = max(dp[i-1],maxi+prices[i]);
+    		maxi = max(maxi,dp[i-1]-prices[i]);
+    	}    
+    	return dp[prices.size()-1];
     }
 };
