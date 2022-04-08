bbec7b576eb8a3cdec476398a8236bfb8786b57d
dikshant bansal <dikshantbansal98@gmail.com>
2022-04-08 19:58:52 +0530

    Time: 713 ms (25.28%), Space: 125.1 MB (39.75%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    map<int,int>hash;
+    map<int,int>ranges;
     int overLapAmount(pair<int,int>segment1, pair<int,int>segment2) {
         if(segment1.first >= segment2.second || segment2.first >= segment1.second) {
             return 0;
@@ -8,31 +8,36 @@ public:
         return min(segment1.second,segment2.second) - max(segment1.first,segment2.first);
     }
     
-    pair<int,int> merge(pair<int,int>segment1, pair<int,int>segment2) {
-        return {min(segment1.first,segment2.first), max(segment1.second,segment2.second)};
+    map<int,int>::iterator getPosition(int left, int right) {
+        auto position = ranges.upper_bound(left);
+        if(position != ranges.begin()) {
+            position--;
+        }
+        if(position != ranges.end() && position->second < left) {
+            position++;
+        }
+        return position;
     }
-    
+    //0-4
+    //
     vector<int> amountPainted(vector<vector<int>>& paints) {
         vector<int>result;
         for(vector<int>&paint: paints) {
-            int amount = 0;
-            auto position = hash.upper_bound(paint[0]);
-            if(position != hash.begin()) {
-                position = prev(position);
-            }
-            int start = paint[0], end = paint[1];
-            while(position!=hash.end() && position->first < paint[1]) {
+            int amount = 0, start = paint[0], end = paint[1];
+            auto position = getPosition(start, end);
+            while(position!=ranges.end() && position->first < paint[1]) {
                 int overlap = overLapAmount({paint[0],paint[1]}, *position);
-                if(overlap > 0) {
+                if(overlap >= 0) {
                     amount += overlap;
-                    auto [first,second] = merge({start,end}, *position);
-                    start = first, end = second;
-                    auto copyIterator = position;
-                    hash.erase(copyIterator);
+                    start = min(start, position->first);
+                    end = max(end, position->second);
+                    ranges.erase(position++);
                 }
-                position++;
+                else {
+                    position++;
+                }
             }
-            hash[start] = end;
+            ranges[start] = end;
             result.push_back(paint[1]-paint[0]-amount);
         }
         return result;
