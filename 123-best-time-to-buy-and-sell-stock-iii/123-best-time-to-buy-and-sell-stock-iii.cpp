2493e5e0836ee3cbd75ffd1cca7b4c0766de9311
dikshant bansal <dikshantbansal98@gmail.com>
2022-04-09 15:29:41 +0530

    Time: 144 ms (87.94%), Space: 81.5 MB (43.62%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<int>maximum(prices.size()),dp(prices.size());
+        for(int i = prices.size()-1;i >= 0; --i)
+        {
+            if(i==prices.size()-1)
+                maximum[i] = prices[i];
+            else
+                maximum[i] = max(prices[i],maximum[i+1]);
+        }
+        int minimum = prices[0];
+        int ans = 0;
+        for(int i = 1; i < prices.size()-1; ++i)
+        {
+            dp[i] = max(prices[i]-minimum,dp[i-1]);
+            ans = max({ans,dp[i-1]+maximum[i+1]-prices[i],dp[i]});
+            minimum = min(prices[i],minimum);
+        }
+        ans = max(ans,prices[prices.size()-1]-minimum);
+        return ans;
+    }
+};
